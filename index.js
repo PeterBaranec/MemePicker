dd5403bb84090dba18269c4270b00350dc27f3ad
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-27 19:31:10 +0100

    get matching cats array

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
 
 getImageBtn.addEventListener("click", getMatchingCatsArray);
 
 function getMatchingCatsArray() {
-  const checkedRadio = document.querySelector("input[type='radio']:checked");
-  const selectedEmotion = document.querySelector(
-    "input[type='radio']:checked"
-  ).value;
-  if (checkedRadio) {
-    console.log(selectedEmotion);
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector(
+      'input[type="radio"]:checked'
+    ).value;
+    const isGif = gifsOnlyOption.checked;
+
+    const matchingCatsArray = catsData.filter(function (cat) {
+      if (isGif) {
+        return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
+      } else {
+        return cat.emotionTags.includes(selectedEmotion);
+      }
+    });
+
+    return matchingCatsArray;
   }
 }
-
 emotionRadios.addEventListener("change", highlightCheckedOption);
 
 function highlightCheckedOption(e) {
