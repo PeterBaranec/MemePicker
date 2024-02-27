2af8571b07aa6ea6702321ca4fb6e1be800480da
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-27 18:23:03 +0100

    getting value of input radio when clicked

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
+function getMatchingCatsArray() {
+  const checkedRadio = document.querySelector("input[type='radio']:checked");
+  const selectedEmotion = document.querySelector(
+    "input[type='radio']:checked"
+  ).value;
+  if (checkedRadio) {
+    console.log(selectedEmotion);
+  }
+}
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
 
